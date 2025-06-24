7dc800f43b18e6d848592758320e8a71d8004cb2
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-06-23 23:14:59 -0600

    Guia 1 singleton desde 0

---
+++ Codigo Limpio/Clase1Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo_Limpio
+{
+    internal class Clase1Singleton
+    {
+       
+        public static Clase1Singleton instancia;
+
+        private Clase1Singleton()
+        {
+            // Constructor privado para evitar instanciación externa
+        }
+        public static Clase1Singleton getInstancia() {
+            if(instancia == null)
+            {
+                instancia = new Clase1Singleton();
+            }
+            return instancia;
+        }
+
+        
+
+    }
+}
---
+++ Codigo Limpio/Ejecucion.cs
@@ -0,0 +1,6 @@
+﻿
+using Codigo_Limpio;
+
+Console.WriteLine("Hola mundo");
+Clase1Singleton instancia = Clase1Singleton.getInstancia();
+Console.WriteLine(instancia.GetType());
