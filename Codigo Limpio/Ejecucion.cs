9f6622c2fd0a7c55186146e4ac88af5785684baa
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-06-24 23:03:22 -0600

    Guia 1 Prueba sobre singleton eventLog

--- Codigo Limpio/Ejecucion.cs
+++ Codigo Limpio/Ejecucion.cs
@@ -1,5 +1,6 @@
 ﻿
 using Codigo_Limpio;
+using Codigo_Limpio.prueba;
 #region SingletonSinId
 Console.WriteLine("Hola mundo");
 Clase1Singleton instancia = Clase1Singleton.getInstancia();
@@ -14,6 +15,20 @@ SingretonID instanciaID3 = SingretonID.getInstancia();
 Console.WriteLine(instanciaID.Id);
 Console.WriteLine(instanciaID2.Id);
 Console.WriteLine(instanciaID3.Id);
+#endregion
+
+#region EjemploSingletonLog
+
+EventLogger logger = EventLogger.GetInstance();
+logger.LogEvent("Inicio de la aplicación");
+logger.LogEvent("Se ha realizado una operación importante");
+EventLogger logger2 = EventLogger.GetInstance();
+logger2.LogEvent("Se ha realizado otra operación importante en la base de datos esperando");
+logger2.LogEvent("Se ha realizado otra operación importante Asyncrona");
+logger2.LogEvent("Consulta exitosa pasamos al siguiente dato");
+logger.LogEvent("Se ha realizado una operación de cierre de sesión");
+logger.LogEvent("Fin de la aplicación");
+logger.MostrarLogs();
 
 
 #endregion
